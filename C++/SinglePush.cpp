6b063be53106cc8ebe55312fb3981cc3dde4f8ca
Kamal Adel <47852010+kamal1491644@users.noreply.github.com>
2021-08-20 15:38:01 +0200

    Update SinglePush.cpp

--- C++/SinglePush.cpp
+++ C++/SinglePush.cpp
@@ -1,83 +1,52 @@
 #include <iostream>
-#include<deque>
 #include <set>
 #include<algorithm>
 using namespace std;
-// LineLand Mail Problem
-
+const int size = 1e5;
 int main()
 {
-    int t;
+    int t,a[size],b[size];
     cin>>t;
-
-    for(int i=0;i<t;i++){
+    while (t--)
+    {
         bool flag=1;
-       int firstIndex=-1;
-       int lastIndex=-1;
-        int temp=0;
-        int n;
+        int n,firstIndex=-1,lastIndex=-1;
         cin>>n;
-
-        int a[n];
-        int b[n];
-
-        for(int j=0;j<n;j++){
+        for(int j=0;j<n;j++)
             cin>>a[j];
-        }
-
-        for(int j=0;j<n;j++){
+        for(int j=0;j<n;j++)
             cin>>b[j];
-        }
-
-        // compare the difference between the two arrays
-        // found first index
-        for(int i=0;i<n;i++){
-            if (a[i]>b[i]){
-               flag=0;
-            }
-            if (a[i]!=b[i]){
-                temp=a[i]-b[i];
-                firstIndex=i;
-                break;
+        for(int i=0;i<n;i++)
+        {
+            if (a[i]>b[i])
+                flag=0;
+            if (a[i]!=b[i])
+            {
+                if(firstIndex==-1)
+                    firstIndex = i;
+                lastIndex = i;
             }
         }
-
-
-        // found last index
-
-        for(int i=0;i<n;i++){
-            if (a[i]!=b[i]){
-                temp=a[i]-b[i];
-                lastIndex=i;
-            }
-        }
-
-        // if first is -1
-        if (firstIndex==-1){
+        if(firstIndex==-1)
             cout<<"YES"<<endl;
-        }else{
+        else
+        {
             set<int>st;
-            for(int i=firstIndex;i<=lastIndex;i++){
+            for(int i=firstIndex;i<=lastIndex;i++)
+            {
                 int diff=a[i]-b[i];
                 st.insert(diff);
             }
-
-            if (!flag)
+            if(!flag)
                 cout<<"NO"<<endl;
-            else{
-                if (st.size()==1){
+            else
+            {
+                if (st.size()==1)
                     cout<<"YES"<<endl;
-                }else
+                else
                     cout<<"NO"<<endl;
-
             }
-
-
         }
-
-
-
-
     }
     return 0;
 }
